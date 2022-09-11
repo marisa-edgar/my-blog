eabbab7726cf00701b9329c10706ef572c3acdc0
marisa-edgar <92288470+marisa-edgar@users.noreply.github.com>
2022-09-11 05:15:14 -0700

    add temp json server

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,19 +1,12 @@
-
-
-const BlogList = ({ blogs, title }) => {
-  // const blogs = props.blogs;
-  // const title = props.title;
-
-
-
-
-  return ( 
+const BlogList = ({ blogs, title, handleDelete }) => {
+  return (
     <div className="blog-list">
       <h2>{ title }</h2>
-      { blogs.map((blog) =>(
-        <div className='blog-preview' key={blog.id}>
+      {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id} >
           <h2>{ blog.title }</h2>
-          <p>Written by: { blog.author }</p>
+          <p>Written by { blog.author }</p>
+          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,36 @@
-import { useState } from 'react';
+
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'Blog1', body: 'lorem ipsum...', author: 'ME', id:1 },
-    { title: 'Blog2', body: 'lorem ipsum...', author: 'ME', id:2 },
-    { title: 'Blog3', body: 'lorem ipsum...', author: 'ME', id:3 },
+    { title: 'Blog1', category: 'JavaScript', body: 'lorem ipsum...', author: 'ME', id:1 },
+    { title: 'Blog2', category: 'React', body: 'lorem ipsum...', author: 'ME', id:2 },
+    { title: 'Blog3', category: 'TypeScript', body: 'lorem ipsum...', author: 'ME', id:3 },
+    { title: 'Blog4', category: 'JavaScript', body: 'lorem ipsum...', author: 'ME', id:4 },
+    { title: 'Blog5', category: 'React', body: 'lorem ipsum...', author: 'ME', id:5 },
+    { title: 'Blog6', category: 'TypeScript', body: 'lorem ipsum...', author: 'ME', id:6 },
   ]);
+  
+  const [category, setCategory] = useState('JavaScript');
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
+  useEffect(() => {
+    console.log('useEffect run');
+    console.log(category);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [category]);
 
   return ( 
     <div className='home'>
-      <BlogList blogs={ blogs } title= 'All Blogs'/>
+      <BlogList blogs={ blogs } title= 'All Blogs' handleDelete= { handleDelete }/>
+      <p>{ category }</p>
+      <button onClick={() => setCategory('React')}> React</button>
+      <button onClick={() => setCategory('TypeScript')}> TypeScript</button>
     </div>
   );
 }
