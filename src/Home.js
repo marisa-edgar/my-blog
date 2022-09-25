093456fdb59fa7a8ef1589902db5021044090cad
marisa-edgar <92288470+marisa-edgar@users.noreply.github.com>
2022-09-25 04:44:55 -0700

    build routing

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,15 @@
 
-import { useState, useEffect } from 'react';
-import BlogList from './BlogList';
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'Blog1', category: 'JavaScript', body: 'lorem ipsum...', author: 'ME', id:1 },
-    { title: 'Blog2', category: 'React', body: 'lorem ipsum...', author: 'ME', id:2 },
-    { title: 'Blog3', category: 'TypeScript', body: 'lorem ipsum...', author: 'ME', id:3 },
-    { title: 'Blog4', category: 'JavaScript', body: 'lorem ipsum...', author: 'ME', id:4 },
-    { title: 'Blog5', category: 'React', body: 'lorem ipsum...', author: 'ME', id:5 },
-    { title: 'Blog6', category: 'TypeScript', body: 'lorem ipsum...', author: 'ME', id:6 },
-  ]);
-  
-  const [category, setCategory] = useState('JavaScript');
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-
-  useEffect(() => {
-    console.log('useEffect run');
-    console.log(category);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [category]);
-
-  return ( 
-    <div className='home'>
-      <BlogList blogs={ blogs } title= 'All Blogs' handleDelete= { handleDelete }/>
-      <p>{ category }</p>
-      <button onClick={() => setCategory('React')}> React</button>
-      <button onClick={() => setCategory('TypeScript')}> TypeScript</button>
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return (  
-    <nav className='navbar'>
+  return (
+    <nav className="navbar">
       <h1>Marisa's Blog</h1>
-      <div className='links'>
-        <a href='/'>Home</a>
-        <a href='/create'>New Blog</a>
-      
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</Link>
       </div>
     </nav>
   );
 }
-export default Navbar  ;
+
+export default Navbar;
